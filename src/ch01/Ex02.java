88b45fcb9ff0f1761c768dc4a16e368a0fe27503
DESKTOP-DSBGP8J\82107 <kimdohyeong03@naver.com>
2022-06-09 11:24:35 +0900

    Ex02 프로그램 생성 -> 우리가 지금까지 배운 내용 정리

---
+++ src/ch01/Ex02.java
@@ -0,0 +1,19 @@
+package ch01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		System.out.println("1. 깃허브에 가입");
+		System.out.println("2. 소스트리를 설치");
+		System.out.println("3. 소스트리를 둘러보기");
+		System.out.println("4. 깃허브를 둘러보기");
+		System.out.println("5. 혼자서 깃 해보기");
+		System.out.println("5-1. local_2022-06-09 를 레파지토리화");
+		System.out.println("5-2. local_2022-06-09 에 여러 작업");
+		System.out.println("5-3. 커밋, 커밋 메시지에 대해서 배움");
+		System.out.println("5-4. 버전으로 옮겨다니면서 과거의 작업으로 돌아갈 수 있다는 걸 배움");
+		System.out.println("5-5. ");
+		System.out.println("5-6. gitignore 에서 class 파일 무시 ");
+		
+	}
+}
